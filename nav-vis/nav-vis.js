1d8d7f0be1386e889c2a50839fc8eb4106b8b7fb
Paul Abrams <pabrams@gmail.com>
2019-08-08 10:34:15 -0700

    Update nav-vis.js

--- nav-vis/nav-vis.js
+++ nav-vis/nav-vis.js
@@ -538,14 +538,13 @@ li.navjs-widget-spacer a {
   border-radius: 3px;
   background-color: #d7f5f4;
 }
-.navjs-mid.nav-pills li a {
-  padding: 15px 0 8px 0;
+.navjs-mid.nav-pills li {
+  border-radius: 0;
   border-bottom: 1px solid #e7e7e7;
-  background-color: #fff;
 }
 .navjs-mid.nav-pills li.active a,
 .navjs-mid.nav-pills li.active a:hover {
-  padding: 15px 0 9px 0;
+  padding-bottom: 8px;
   border-bottom: 2px solid #d7f5f4;
   background-color: #fff;
   font-weight: bold;
