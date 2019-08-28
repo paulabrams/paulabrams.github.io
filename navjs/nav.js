98341648da825254271e0402563fc20cc534e2ea
Paul Abrams <pabrams@gmail.com>
2019-08-28 10:35:07 -0700

    Update nav.js

--- navjs/nav.js
+++ navjs/nav.js
@@ -75,9 +75,9 @@ looker.plugins.visualizations.add({
       if (nav.widget === "hidden") { continue }
 
       // Active Tab
-      if (nav.style === "active_param" && navjs.active_param_value !== '') {
+      if (nav.style === "active_param" && nav.active_param_value !== '') {
         if (navjs.data && navjs.data[0]) {
-          if ((''+navjs.data[0][nav.active_param]) === (''+navjs.active_param_value)) {
+          if ((''+navjs.data[0][nav.active_param].value) === (''+nav.active_param_value)) {
             nav.style = "active"
           }
         }
