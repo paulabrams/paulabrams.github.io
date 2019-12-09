d5ea825e898c37e9eda8443eac0e1410daf6978f
Paul Abrams <pabrams@gmail.com>
2019-12-09 10:02:04 -0800

    Spot

---
+++ spot/index.md
@@ -0,0 +1,11 @@
+# Spot.js
+### Web Tracker Tag
+Spot.js is a web tracker tag
+
+## Feature Overview
+**Device Token** - Creates a unique device token cookie on start.
+
+## Usage
+Install Spot on your page manually or via your tag manager.
+
+## Setup
---
+++ spot/spot.js
@@ -0,0 +1,18 @@
+/**
+ *  spot.js
+ *
+ *  Spot.js is a web tracker tag
+ *
+ *  hosts
+ *    https://paulabrams.github.io/spot/spot.js
+ 
+ */
+var spotjs = {
+  version: "0.0.1",
+  name: "spot-"+Math.random().toString(36).substring(7),
+  fieldJson: '',
+  dataJson: '',
+  configJson: ''
+}
+
+console.log (spotjs.name, "says hello")
