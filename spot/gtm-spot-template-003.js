58e74c11cba5e251de89ff5d0eebd37532bb4cb9
Paul Abrams <pabrams@gmail.com>
2020-01-06 15:07:34 -0800

    external script poc

---
+++ spot/gtm-spot-template-003.js
@@ -0,0 +1,169 @@
+/**
+ *  Spot Sandbox Template
+ *  This is a sandbox-based version of Spot.js.
+ *  This is the body of a function with a single parameter, data, i.e.
+ *    function spot(data) { ... }
+ */
+let spot = { dt: null, ut: null };
+
+const log = require('logToConsole');
+log('spot template 0.0.5');
+log('data =', data);
+
+
+//
+// Require
+//
+const queryPermission = require('queryPermission');
+
+const addEventCallback = require('addEventCallback');
+
+const generateRandom = require('generateRandom');
+
+const getCookieValues = require('getCookieValues');
+const setCookie = require('setCookie');
+
+const getReferrerUrl = require('getReferrerUrl');
+const getUrl = require('getUrl');
+
+//const copyFromWindow = require('copyFromWindow');
+const encodeUriComponent = require('encodeUriComponent');
+//const encodeUri = require('encodeUri');
+const createQueue = require('createQueue');
+const copyFromWindow = require('copyFromWindow');
+const sendPixel = require('sendPixel');
+
+//
+// Data Layer
+//
+let spotData = [];
+  if (queryPermission('access_globals', 'readwrite', 'spotData')) {
+  spot.dataLayerPush = createQueue('spotData');
+  spot.dataLayerPush({ "data": data });
+  spotData = copyFromWindow('spotData');
+  //log ("spotData =", spotData);
+}
+//
+// Inject Script
+//
+if (data.scriptUrl) {
+  const injectScript = require('injectScript');
+  if (queryPermission('inject_script', data.scriptUrl)) {
+    if (spotData.length === 1) {
+      injectScript(data.scriptUrl);
+    }
+  }
+}
+
+//
+// Page Referer
+//
+spot.getReferrer = function(paramName) {
+  let paramValue;
+  if (queryPermission('get_referrer', paramName)) {
+    paramValue = getReferrerUrl(paramName);
+    log('referrer', paramName, '=', paramValue);
+  }
+  return paramValue;
+};
+//spot.getReferrer();
+
+//
+// Page Location
+//
+spot.getURL = function(paramName) {
+  let paramValue;
+  if (queryPermission('get_url', paramName)) {
+    paramValue = getUrl(paramName);
+    log('url', paramName, '=', paramValue);
+  }
+  return paramValue;
+};
+//spot.getURL();
+
+// 
+// Cookies
+//
+spot.getCookie = function(cookieName) {
+  let cookieValues = null,
+      cookieValue = null;
+  if (queryPermission('get_cookies', cookieName)) {
+    cookieValues = getCookieValues(cookieName);
+    log('getCookie', cookieName, '=', cookieValues);
+    if (cookieValues && cookieValues[0]) {
+      cookieValue = cookieValues[0];
+      log('getCookie', cookieName, '=', cookieValue);
+    }
+  }
+  return cookieValue;
+};
+
+spot.setCookie = function(cookieName, cookieValue, options) {
+  if (queryPermission('set_cookies', cookieName, options)) {
+    setCookie(cookieName, cookieValue, options);
+    log('setCookie', cookieName, '=', cookieValue);
+  }
+};
+
+spot.generateToken = function (prefix) {
+  return (prefix||'')+generateRandom(10000000000, 1000000000000);
+};
+
+
+// Cookie tests
+spot.dt = spot.getCookie(data.cookiePrefix+'dt');
+if (!spot.dt) {
+  spot.dt = spot.generateToken('dt');
+  log('generating token', spot.dt, ' for host =', spot.getURL('host'));
+  spot.setCookie(data.cookiePrefix+'dt', spot.dt, {
+    'domain': data.cookieDomain || spot.getURL('host'),
+    'path': '/',
+    'max-age': 60*60*24*365,
+    'secure': true
+  });
+}
+ 
+//
+// Call Event API
+//
+spot.callEventsApi = function (apiEndpoint, apiParams, opts) {
+  let url = data.apiHost+apiEndpoint;
+  const last = url.charAt(url.length - 1);
+  let delimiter = '&';
+  if (url.indexOf('?') < 0) {
+    delimiter = '?';
+  } else if (last == '?' || last == '&') {
+    delimiter = '';
+  }
+
+  apiParams = apiParams || {};
+  for (const p in apiParams) {
+    if (p && apiParams[p]) {
+      url += delimiter+encodeUriComponent(p)+'='+encodeUriComponent(apiParams[p]);
+      delimiter = '&';
+    }
+  }
+
+  opts = opts || {};
+  opts.onSuccess = opts.onSuccess || data.gtmOnSuccess;
+  opts.onFailure = opts.onFailure || data.gtmOnFailure;
+
+  log("spot.callEventsApi", url);
+  sendPixel(url, opts.onSuccess, opts.onFailure);
+};
+
+//
+// Submit Event
+//
+let apiParams = { "dt": spot.dt, "ut": spot.ut, "eventType": data.eventType };
+if (data.eventParams) {
+  data.eventParams.forEach(function(p) {
+    if (p.key && p.value) {
+      apiParams[p.key] = p.value;
+    }
+  });
+}
+spot.callEventsApi("/spot", apiParams);
+
+// Call data.gtmOnSuccess when the tag is finished.
+data.gtmOnSuccess();
--- spot/spot.js
+++ spot/spot.js
@@ -1,27 +1,83 @@
 /**
- *  spot.js
+ * Spot.js 0.0.3
  *
  *  Spot.js is a web tracker tag
  *
+ *  Spot observes window.spotDataLayer array
+ *
  */
+
+//
+// Default Config
+//
+var spotConfig = {
+  apiHost: "https://growingtree.demostellar.com",
+  apiEndpoint: "/edp/api/event",
+  apiAuthorization: "Bearer 7ed9828b0021035c22f1b142db14704bc4eb95b11f93d973bd9c9b698cf736e4:3e1824ff3ec2d7e2e20c13fa00d60d4dbc4a965d5fd48a1f4887338759c1d8e7"
+};
+
+//
+// Implementation
+//
 var spotjs = {
-  version: "0.0.2",
-  name: "spot-"+Math.random().toString(36).substring(7),
-  fieldJson: '',
-  dataJson: '',
-  configJson: ''
+  name: "spot-0.0.3-"+Math.random().toString(36).substring(7),
+  config: spotConfig,
+  dataLayer: null
 }
 
+spotjs.onDataLayerPush = function (arr) {
+  console.log("spotjs.onDataLayerPush", arr);
+  spotjs.processDataLayer(arr);
+}
 
-
-spotjs.main = function () {
-  // process data layer queue
-  console.log("spotjs.main spotData =", spotData)
-  while(spotData.length) {
-    var evt = spotData.pop();
-    console.log("evt =", evt)
+spotjs.processDataLayer = function () {
+  if (spotjs.onDataLayerPush) {
+    do while (spotjs.dataLayer.length) {
+      let item = spotjs.dataLayer.pop();
+      spotjs.processEvent(item);
+    }
   }
 }
 
-console.log (spotjs.name, "loaded", spotjs)
-window.setInterval(spotjs.main, 1000)
+spotjs.processEvent = function (data) {
+  // process data layer queue
+  if (typeof data !== "object") {
+    console.log("spotjs.main spotData skipping non-object", data)
+    return;
+  }
+  console.log("spotjs.processEvent data =", data)
+  data.meta = {};
+  if (!data.client) { data.client = { "identifier": { "id": "rasilang@gmail.com", "id_field": "email" } } }
+  if (!data.event) { data.event = "event": { "type": "bounce", "iso_time": "2019-12-05T00:00:00.000Z" } }
+  spotjs.submitEvent(data)
+}
+
+spotjs.submitEvent (data) {
+  console.log("spotjs.submitEvent data =", data)
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", spotConfig.apiHost+spotConfig.apiEndpoint, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("Authorization", spotConfig.apiAuthorization);
+  xhr.onreadystatechange = function() {
+    console.log("spotjs.submitEvent XHR finished", this);
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      console.log("spotjs.submitEvent XHR finished", this);
+    }
+  }
+  xhr.send(data);
+}
+
+// Init Data Layer
+if (!spotjs.dataLayer) {
+  if (typeof window.spotDataLayer === 'undefined') {
+    window.spotDataLayer = [];
+  }
+  spotjs.dataLayer = window.spotDataLayer;
+  spotjs.dataLayer.push = function(e) {
+    Array.prototype.push.call(arr, e);
+    spotjs.onDataLayerPush(arr);
+  };
+  spotjs.processDataLayer(arr);
+}
+
+console.log (spotjs.name, "loaded")
